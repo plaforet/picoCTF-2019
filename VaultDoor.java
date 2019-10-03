8597f043e6e8e8463448c8695159100b5a255277
plaforet <plaforet@houstonisd.org>
2019-10-03 14:21:47 -0500

    VaultDoor7 DONE
    I have a headache.

--- VaultDoor.java
+++ VaultDoor.java
@@ -3,8 +3,8 @@ import java.net.URLDecoder;
 import java.util.*;
 
 public class VaultDoor {
-	// Solutions to Vault Door 1 & 3
 
+	// Vault Door 1
 	public static void vaultDoor1() {
 		char[] password = new char[32];
 		password[0] = 'd';
@@ -44,6 +44,7 @@ public class VaultDoor {
 		}
 	}
 
+	// Vault Door 3
 	public static String vaultDoor3() {
 		String password = "jU5t_a_sna_3lpm13g34c_u_4_m3rf48";
 		char[] buffer = new char[32];
@@ -65,10 +66,14 @@ public class VaultDoor {
 		return s;
 	}
 
+	// Vault Door 4
 	public static String vaultDoor4() {
 
-		byte[] myBytes = { 106, 85, 53, 116, 95, 52, 95, 98, 0x55, 0x6e, 0x43, 0x68, 0x5f, 0x30, 0x66, 0x5f, 0142, 0131,
-				0164, 063, 0163, 0137, 067, 0141, '1', 'c', '8', 'c', '6', '6', '8', 'b', };
+		byte[] myBytes = { 106, 85, 53, 116, 95, 52, 95, 98,
+				0x55, 0x6e, 0x43, 0x68, 0x5f, 0x30, 0x66,
+				0x5f, 0142, 0131, 0164, 063, 0163, 0137,
+				067, 0141, '1', 'c', '8', 'c', '6', '6',
+				'8', 'b', };
 		String password;
 		try {
 			password = new String(myBytes, "UTF-8");
@@ -84,48 +89,46 @@ public class VaultDoor {
 	// (A better GUI tool can be found at https://encoding.tools)
 	public static String vaultDoor5() {
 		String expected = "JTYzJTMwJTZlJTc2JTMzJTcyJTc0JTMxJTZlJTY3JTVm"
-				+ "JTY2JTcyJTMwJTZkJTVmJTYyJTYxJTM1JTY1JTVmJTM2" + 
-				"JTM0JTVmJTY0JTYyJTM2JTM5JTM0JTM2JTYyJTYx";
+				+ "JTY2JTcyJTMwJTZkJTVmJTYyJTYxJTM1JTY1JTVmJTM2"
+				+ "JTM0JTVmJTY0JTYyJTM2JTM5JTM0JTM2JTYyJTYx";
 		return urlDecode(base64Decode(expected));
 	}
-	
+
+	// Vault Door 6
 	public static String vaultDoor6() {
-		byte[] myBytes = {
-	            0x3b, 0x65, 0x21, 0xa , 0x38, 0x0 , 0x36, 0x1d,
-	            0xa , 0x3d, 0x61, 0x27, 0x11, 0x66, 0x27, 0xa ,
-	            0x21, 0x1d, 0x61, 0x3b, 0xa , 0x2d, 0x65, 0x27,
-	            0xa , 0x34, 0x30, 0x31, 0x30, 0x36, 0x30, 0x31,
-	        };
+		byte[] myBytes = { 0x3b, 0x65, 0x21, 0xa, 0x38, 0x0,
+				0x36, 0x1d, 0xa, 0x3d, 0x61, 0x27, 0x11,
+				0x66, 0x27, 0xa, 0x21, 0x1d, 0x61, 0x3b,
+				0xa, 0x2d, 0x65, 0x27, 0xa, 0x34, 0x30,
+				0x31, 0x30, 0x36, 0x30, 0x31, };
 		byte[] passBytes = new byte[32];
-		
-		for(int i=0; i<passBytes.length; i++) {
+
+		for (int i = 0; i < passBytes.length; i++) {
 			passBytes[i] = (byte) (myBytes[i] ^ 0x55);
 		}
-		
+
 		try {
 			return new String(passBytes, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			System.out.println("Unsupported Encoding");
 			return null;
 		}
-		
+
 	}
 
+	// Encode String to Base64
 	public static String base64Encode(byte[] input) {
 		return Base64.getEncoder().encodeToString(input);
 	}
 
+	// Decode Base64 to String
 	public static String base64Decode(String base) {
-		byte[] decodedURLBytes = Base64.getUrlDecoder().decode(base);
+		byte[] decodedURLBytes = Base64.getUrlDecoder()
+				.decode(base);
 		return new String(decodedURLBytes);
 	}
 
-	// URL encoding is meant for web pages, so any double agent spies who steal
-	// our source code will think this is a web site or something, defintely not
-	// vault door! Oh wait, should I have not said that in a source code
-	// comment?
-	//
-	// -Minion #2415
+	// Convert an array of bytes into URL encoding
 	public static String urlEncode(byte[] input) {
 		StringBuffer buf = new StringBuffer();
 		for (int i = 0; i < input.length; i++) {
@@ -134,46 +137,141 @@ public class VaultDoor {
 		return buf.toString();
 	}
 
+	// Decode URL encoded text into UTF formatted String
 	public static String urlDecode(String encodedURLString) {
 		try {
-			return URLDecoder.decode(encodedURLString, "UTF-8");
+			return URLDecoder.decode(encodedURLString,
+					"UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			System.out.println("Unsupported Encoding.");
 			return null;
 		}
-		
+
 	}
 
-	
+	/*
+	 * Each character can be represented as a byte value using its ASCII encoding.
+	 * Each byte contains 8 bits, and an int contains 32 bits, so we can "pack" 4
+	 * bytes into a single int. Here's an example: if the hex string is "01ab", then
+	 * those can be represented as the bytes {0x30, 0x31, 0x61, 0x62}. When those
+	 * bytes are represented as binary, they are:
+	 * 
+	 * 0x30: 00110000 0x31: 00110001 0x61: 01100001 0x62: 01100010
+	 * 
+	 * If we put those 4 binary numbers end to end, we end up with 32 bits that can
+	 * be interpreted as an int.
+	 * 
+	 * 00110000001100010110000101100010 -> 808542562
+	 * 
+	 * Since 4 chars can be represented as 1 int, the 32 character password can be
+	 * represented as an array of 8 ints.
+	 * 
+	 * - Minion #7816
+	 */
+	public int[] passwordToIntArray(String hex) {
+		int[] x = new int[8];
+		byte[] hexBytes = hex.getBytes();
+		for (int i = 0; i < 8; i++) {
+			x[i] = hexBytes[i * 4] << 24
+					| hexBytes[i * 4 + 1] << 16
+					| hexBytes[i * 4 + 2] << 8
+					| hexBytes[i * 4 + 3];
+		}
+		return x;
+	}
 
-	// Dr. Evil gave me a book called Applied Cryptography by Bruce Schneier,
-    // and I learned this really cool encryption system. This will be the
-    // strongest vault door in Dr. Evil's entire evil volcano compound for sure!
-    // Well, I didn't exactly read the *whole* book, but I'm sure there's
-    // nothing important in the last 750 pages.
-    //
-    // -Minion #3091
-    public boolean checkPassword(String password) {
-        if (password.length() != 32) {
-            return false;
-        }
-        byte[] passBytes = password.getBytes();
-        byte[] myBytes = {
-            0x3b, 0x65, 0x21, 0xa , 0x38, 0x0 , 0x36, 0x1d,
-            0xa , 0x3d, 0x61, 0x27, 0x11, 0x66, 0x27, 0xa ,
-            0x21, 0x1d, 0x61, 0x3b, 0xa , 0x2d, 0x65, 0x27,
-            0xa , 0x34, 0x30, 0x31, 0x30, 0x36, 0x30, 0x31,
-        };
-        for (int i=0; i<32; i++) {
-            if (((passBytes[i] ^ 0x55) - myBytes[i]) != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static String[] stringBinary() {
+		int[] intArray = new int[8];
+		intArray[0] = 1096770097;
+		intArray[1] = 1952395366;
+		intArray[2] = 1600270708;
+		intArray[3] = 1601398833;
+		intArray[4] = 1716808014;
+		intArray[5] = 1734293602;
+		intArray[6] = 1701067056;
+		intArray[7] = 892756537;
+
+		String[] stringBinary = new String[8];
+		for (int i = 0; i < intArray.length; i++) {
+			stringBinary[i] = Integer
+					.toBinaryString(intArray[i]);
+		}
+		return stringBinary;
+
+	}
+
+	public static String[][] parsedArray(String[] stringBinary) {
+		String[][] parsedArray = new String[8][4];
+		for (int i = 0; i < 8; i++) {
+			String binary = stringBinary[i];
+			int len = binary.length();
+			for (int j = 3; j > 0; j--) {
+				parsedArray[i][j] = binary
+						.substring(len - 8);
+				binary = binary.substring(0, len - 8);
+				len -= 8;
+			}
+			parsedArray[i][0] = binary;
+		}
+		return parsedArray;
+	}
+
+	// Convert binary 2D Array to Decimal 2D array
+	public static byte[][] binaryToByte(String[][] binaryArray) {
+		byte[][] byteArray = new byte[8][4];
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 4; j++) {
+				byteArray[i][j] = Byte
+						.parseByte(binaryArray[i][j], 2);
+				System.out.print(byteArray[i][j] + ", ");
+			}
+			System.out.println();
+		}
+		return byteArray;
+	}
+
+	public static String byteToString(byte[][] byteArray) {
+		String password = "";
+		for (int i = 0; i < 8; i++) {
+			try {
+				String pwRow = null;
+				for (int j = 0; j < 4; j++) {
+
+					pwRow = new String(byteArray[i],
+							"UTF-8");
+				}
+				password += pwRow;
+			} catch (UnsupportedEncodingException e) {
+
+				e.printStackTrace();
+			}
+
+		}
+
+		return password;
+
+	}
+
+	// Latest VaultDoor checkPassword
+	public boolean checkPassword(String password) {
+		if (password.length() != 32) {
+			return false;
+		}
+		int[] x = passwordToIntArray(password);
+		return x[0] == 1096770097 && x[1] == 1952395366
+				&& x[2] == 1600270708 && x[3] == 1601398833
+				&& x[4] == 1716808014 && x[5] == 1734293602
+				&& x[6] == 1701067056 && x[7] == 892756537;
+	}
 
 	public static void main(String[] args) {
-		System.out.println(vaultDoor6());
+
+		// Parse the long Binary Strings into a multidimensional array
+		String[][] binary2DArray = parsedArray(
+				stringBinary());
+		byte[][] byteArray = binaryToByte(binary2DArray);
+		System.out.println(byteToString(byteArray));
+
 	}
 
 }
